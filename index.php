7d689af018b24c5385ddb11b0df22d3d5f6fb23d
Andriangaming1945 <anrianwibowo06@gmail.com>
2023-04-07 14:51:12 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h1>Macini</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur voluptates quaerat optio ipsam assumenda aut ipsum iusto iure molestiae saepe omnis harum recusandae corrupti cumque, nulla voluptatem eum quia in.</p>
+</body>
+</html>
